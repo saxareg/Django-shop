551e126c467063f7a3efecbbb989d02342e7f37f
Ivan <saxareg2007@gmail.com>
2025-08-07 11:04:36 +0300

    Making class product

--- project/shop/models.py
+++ project/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    discription = models.CharField(max_length=2000)
+    amount = models.IntegerField()
+    price = models.IntegerField()
+    tipe = models.CharField(max_length=255)
+
+
+
