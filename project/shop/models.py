b97f3dd4d40e22294a3c0a8b4a30a365171322ba
Ivan <saxareg2007@gmail.com>
2025-08-07 19:10:03 +0300

    Adding typs and relate it with product

--- project/shop/models.py
+++ project/shop/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     discription = models.CharField(max_length=2000)
     amount = models.IntegerField()
     price = models.IntegerField()
-    tipe = models.CharField(max_length=255)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True)
 
-
-
+    def __str__(self):
+        return self.name
